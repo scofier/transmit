fbda3a15a58a2bda7c2d7d7683c42fe26c556ae9
hejiaxuan7 <hejiaxuan7@sina.com>
2019-09-05 14:53:47 +0800

    修改bug

--- src/main/java/com/hebaibai/ctrt/transmit/util/param/PostJsonParam.java
+++ src/main/java/com/hebaibai/ctrt/transmit/util/param/PostJsonParam.java
@@ -1,11 +1,13 @@
 package com.hebaibai.ctrt.transmit.util.param;
 
+import com.hebaibai.ctrt.convert.reader.DataReader;
 import com.hebaibai.ctrt.convert.reader.JsonDataReader;
 import com.hebaibai.ctrt.transmit.DataType;
 import com.hebaibai.ctrt.transmit.RouterVo;
 import com.hebaibai.ctrt.transmit.util.Param;
 import io.vertx.core.http.HttpMethod;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class PostJsonParam implements Param {
--- src/main/java/com/hebaibai/ctrt/transmit/util/param/PostXmlParam.java
+++ src/main/java/com/hebaibai/ctrt/transmit/util/param/PostXmlParam.java
@@ -1,11 +1,13 @@
 package com.hebaibai.ctrt.transmit.util.param;
 
+import com.hebaibai.ctrt.convert.reader.DataReader;
 import com.hebaibai.ctrt.convert.reader.XmlDataReader;
 import com.hebaibai.ctrt.transmit.DataType;
 import com.hebaibai.ctrt.transmit.RouterVo;
 import com.hebaibai.ctrt.transmit.util.Param;
 import io.vertx.core.http.HttpMethod;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class PostXmlParam implements Param {
