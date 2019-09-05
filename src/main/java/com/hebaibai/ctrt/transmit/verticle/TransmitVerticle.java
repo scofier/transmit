57d7e93b5d0a52954ee69d4d9cf2d6a7ca087310
hejiaxuan7 <hejiaxuan7@sina.com>
2019-09-05 18:02:33 +0800

    修改日志

--- src/main/java/com/hebaibai/ctrt/transmit/verticle/TransmitVerticle.java
+++ src/main/java/com/hebaibai/ctrt/transmit/verticle/TransmitVerticle.java
@@ -11,8 +11,6 @@ import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerRequest;
-import io.vertx.core.logging.JULLogDelegateFactory;
-import io.vertx.core.spi.logging.LogDelegate;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.client.WebClient;
